59b7e7f4933aafa43cdf399e964446d6974f3e05
virza805 <virza805@gmail.com>
2021-08-26 22:32:01 +0600

    curl 23.5 done

---
+++ 23-curl/c2.php
@@ -0,0 +1,13 @@
+<?php  
+// phpinfo();
+// curl.haxx.se/docs/caextract.html
+$ch = curl_init('https://vir-za.com/?v=fbd25224d617'); 
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+$result = curl_exec($ch);
+$error = curl_error($ch);
+if($error) {
+    echo $error;
+}
+
+// 23.6
