0753c926cf40ed71811889340d9e1b5ee05f7657
virza805 <virza805@gmail.com>
2021-10-26 22:20:36 +0600

    2nd time start Laravel

---
+++ 37-laravel/install-37.2.php
@@ -0,0 +1,5 @@
+<?php 
+# cd newapp         | => 1st run this
+# php artisan serve | => 2nd run this & Check to the Browser http://127.0.0.1:8000
+
+
---
+++ 37-laravel/newapp/routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return ['name'=>'world'];
+});
+
+Route::get('/hello/world', function () {
+    return "Hello World";
+});
+
+Route::get('/hello/{name}', function ($worldName) {
+    $worldName = ucwords($worldName);
+    return "Hello {$worldName}";
+});
+
+Route::get('/greet/{greetings}/{worldName}', function ($greetings, $worldName) {
+    $worldName = ucwords($worldName);
+    $greetings = ucwords($greetings);
+    return "Hello {$greetings} {$worldName}";
+});
